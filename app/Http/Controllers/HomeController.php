da30e3b787fea5c0e905e01807aa7c4622964334
vanyavasiliev26-hub <vanya.vasiliev.26@gmail.com>
2026-05-06 13:40:26 +0400

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,19 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Table;
-use App\Models\MenuItem;
-
 class HomeController extends Controller
 {
-    
     public function index()
     {
-        $tables = Table::where('is_active', true)->get();
-        $menuItems = MenuItem::where('is_available', true)->limit(6)->get();
-        
-        return view('welcome', compact('tables', 'menuItems'));
+        return view('welcome');
     }
-    
-    
 }
